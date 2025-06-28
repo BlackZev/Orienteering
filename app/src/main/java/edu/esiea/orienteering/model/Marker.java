97406b5149576465ea9ca3c6362b5561f60e0d36
ABDELKRIM LIMÉRY Zion <166009229+BlackZev@users.noreply.github.com>
2025-06-28 22:45:01 +0200

    add missing logic in models' files

--- app/src/main/java/edu/esiea/orienteering/model/Marker.java
+++ app/src/main/java/edu/esiea/orienteering/model/Marker.java
@@ -1,11 +1,40 @@
 package edu.esiea.orienteering.model;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 // TODO Ajouter les annotations Room nécessaires ainsi que les clefs éventuelles.
+@Entity
 public class Marker {
-    public String name;
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @NonNull private String name = "";
     // TODO définir le champs servant à mémoriser la position de la balise
+    private double latitude;
+    private double longitude;
+
+
+    // constructors
+    public Marker() {};
+    public Marker(@NonNull String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+    // get
+    public Integer getId() { return id; }
+    @NonNull
+    public String getName() { return name; }
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+
+
+    // setter
+    public void setId(int id) { this.id = id; }
+    public void setName(@NonNull String name) { this.name = name; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
 }
--- app/src/main/java/edu/esiea/orienteering/model/Race.java
+++ app/src/main/java/edu/esiea/orienteering/model/Race.java
@@ -1,10 +1,55 @@
 package edu.esiea.orienteering.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 // TODO Ajouter les annotations Room nécessaires ainsi que les clefs éventuelles.
+@Entity
 public class Race {
-    public String name;
-    public String color;
-    public int numberofMarkers;
-    public float minDistance;
-    public float maxDistance;
+    // fields
+    @PrimaryKey
+    @NonNull
+    private int id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String color;
+    @NonNull
+    private int numMarkers;
+    @NonNull
+    private float minDistance;
+    @NonNull
+    private float maxDistance;
+
+
+    // constructors
+    public Race() {};
+    public Race(@NonNull String name, @NonNull String color, @NonNull int numMarkers, @NonNull float minDistance, @NonNull float maxDistance) {
+        setId(id);
+        this.name = name;
+        this.color = color;
+        this.numMarkers = numMarkers;
+        this.minDistance = minDistance;
+        this.maxDistance = maxDistance;
+    }
+
+
+    // get
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getColor() { return color; }
+    public int getNumMarkers() { return numMarkers; }
+    public float getMinDistance() { return minDistance; }
+    public float getMaxDistance() { return maxDistance; }
+
+
+    // setter
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setColor(String color) { this.color = color; }
+    public void setNumMarkers(int numMarkers) { this.numMarkers = numMarkers; }
+    public void setMinDistance(int minDistance) { this.minDistance = minDistance; }
+    public void setMaxDistance(int maxDistance) { this.maxDistance = maxDistance; }
 }
