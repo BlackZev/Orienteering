a7e332efc08d041112f2bee010000e75366802d5
ABDELKRIM LIMÉRY Zion <166009229+BlackZev@users.noreply.github.com>
2025-06-28 22:51:25 +0200

    add viewmodels

---
+++ app/src/main/java/edu/esiea/orienteering/viewmodel/MarkerViewModel.java
@@ -0,0 +1,25 @@
+package edu.esiea.orienteering.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import edu.esiea.orienteering.model.Marker;
+import edu.esiea.orienteering.repository.MarkerRepository;
+
+public class MarkerViewModel extends AndroidViewModel {
+    private MarkerRepository repo;
+    private LiveData<List<Marker>> markers;
+
+    public MarkerViewModel (Application application) {
+        super(application);
+        repo = new MarkerRepository(application);
+        markers = repo.getAll();
+    }
+
+    LiveData<List<Marker>> getAll() { return markers; }
+    public void insert(Marker marker) { repo.insert(marker); }
+}
--- app/src/main/java/edu/esiea/orienteering/viewmodel/RaceViewModel.java
+++ app/src/main/java/edu/esiea/orienteering/viewmodel/RaceViewModel.java
@@ -1,4 +1,25 @@
 package edu.esiea.orienteering.viewmodel;
 
-public class RaceViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import edu.esiea.orienteering.model.Race;
+import edu.esiea.orienteering.repository.RaceRepository;
+
+public class RaceViewModel extends AndroidViewModel {
+    private RaceRepository repo;
+    private LiveData<List<Race>> races;
+
+    public RaceViewModel (Application application) {
+        super(application);
+        repo = new RaceRepository(application);
+        races = repo.getAll();
+    }
+
+    LiveData<List<Race>> getAll() { return races; }
+    public void insert(Race race) { repo.insert(race); }
 }
