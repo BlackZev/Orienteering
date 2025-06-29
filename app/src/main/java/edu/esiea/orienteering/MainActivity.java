2ea0d20b5d132ea79088b7700b3fcbf855b08740
ABDELKRIM LIMÉRY Zion <166009229+BlackZev@users.noreply.github.com>
2025-06-29 11:34:35 +0200

    add user-permissions in AndroidManifest.xml, init osm in MainActivity.java and OSMFragment, delete files unused, change the layout, add string.xml file

--- app/src/main/java/edu/esiea/orienteering/MainActivity.java
+++ app/src/main/java/edu/esiea/orienteering/MainActivity.java
@@ -1,24 +1,76 @@
 package edu.esiea.orienteering;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView map = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.main_osm_fragment);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(20);
+        GeoPoint startPoint = new GeoPoint(43.7194789, -1.0521627);
+        mapController.setCenter(startPoint);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (int i = 0; i < grantResults.length; i++) {
+            permissionsToRequest.add(permissions[i]);
+        }
+        if (!permissionsToRequest.isEmpty()) {
+            int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
--- app/src/main/java/edu/esiea/orienteering/view/OSMFragment.java
+++ app/src/main/java/edu/esiea/orienteering/view/OSMFragment.java
@@ -7,20 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import edu.esiea.orienteering.R;
-
 import org.osmdroid.views.MapView;
 
+import edu.esiea.orienteering.R;
+
 public class OSMFragment extends Fragment {
-    private MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-        mapView = view.findViewById(R.id.map);
+        View view = inflater.inflate(R.layout.main_osm_fragment, container, false);
+        MapView mapView = view.findViewById(R.id.map);
         mapView.setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK);
 
         return view;
     }
-
- }
+}
